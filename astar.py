37947c124b1d32edd908c25c46f8b9c42c1e9981
Leo-H-Cao <cao.leo@yahoo.com>
2021-05-23 15:27:37 -0700

    fixed draw_grid

--- astar.py
+++ astar.py
@@ -153,8 +153,8 @@ def draw_grid(win, rows, width):
     gap = width//rows
     for i in range(rows):
         pygame.draw.line(win, GREY, (0, i*gap), (width, i*gap))
-        for j in range(rows):
-            pygame.draw.line(win, GREY, (j*gap, 0), (j*gap, width))
+    for j in range(rows):
+        pygame.draw.line(win, GREY, (j*gap, 0), (j*gap, width))
 
 def draw(win, grid, rows, width):
     win.fill(WHITE)
